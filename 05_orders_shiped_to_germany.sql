72ad13f297e3d4ab2096619f126c1ff35a6f5ed2
ahmadbelalsidiqi <Ahmad.sidiqiNe@gmail.com>
2026-06-03 10:25:31 -0400

    removed redundant line of code

--- 05_orders_shiped_to_germany.sql
+++ 05_orders_shiped_to_germany.sql
@@ -2,6 +2,4 @@ use northwind;
 select orders.OrderID, orders.ShipName, orders.ShipAddress, shippers.CompanyName
 from orders
          join shippers on orders.ShipVia = shippers.ShipperID
-where OrderID in (select OrderID
-                  from orders
-                  where ShipCountry = 'Germany')
+where  ShipCountry = 'Germany'
